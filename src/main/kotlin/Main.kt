ed43eb400acc7235f581d3fb811fc58d81feec37
wanjirulinahlinah <wainainalinahw@gmail.com>
2023-04-16 20:53:09 +0300

    myarrays

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,27 @@
+fun main() {
+//arrayOf
+    var numArray = arrayOf(10,14,32,6,43,2,75,8)
+    println(numArray[0])
+    println(numArray[1])
+    println(numArray[2])
+    println(numArray[3])
+    println(numArray[4])
+    println(numArray[7])
+
+    var namesarray= arrayOf("Linah","Rose","Mercy","jane","John")
+    println(namesarray[0])
+    println(namesarray[4])
+
+    var countryarray = arrayOf("Kenya","Uganda.","Rwanda","Zambia.")
+    println(countryarray[0])
+    println(countryarray[1])
+    println(countryarray[2])
+    println(countryarray[3])
+    println(countryarray.get(0))
+
+    //add an element to an array
+
+
+
+
+}
